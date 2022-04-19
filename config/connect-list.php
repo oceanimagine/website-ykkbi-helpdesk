655ad0d38d03368af76b64c6749c0e7c51e105be
Ikhsan Bahar <ikhsanbahar@gmail.com>
2022-04-19 17:53:23 +0700

    Kode Helpdesk

--- config/connect-list.php
+++ config/connect-list.php
@@ -9,18 +9,18 @@ $list_server = array(
         "data" => "project_helpdesk",
         "base" => "/helpdesk/"
     ),
-    "chatbotdxb.appazzura.net" => array(
+    "10.8.0.1" => array(
         "host" => "localhost",
-        "user" => "u906809572_helpdesk",
-        "pass" => "Helpdesk@2022",
-        "data" => "u906809572_helpdesk",
-        "base" => "/"
+        "user" => "root",
+        "pass" => "Ykkbi@2021",
+        "data" => "project_helpdesk",
+        "base" => "/helpdesk/"
     ),
-    "appazzura.net" => array(
+    "200.60.4.93" => array(
         "host" => "localhost",
-        "user" => "u906809572_helpdesk",
-        "pass" => "Helpdesk@2022",
-        "data" => "u906809572_helpdesk",
-        "base" => "/"
+        "user" => "root",
+        "pass" => "Ykkbi@2021",
+        "data" => "project_helpdesk",
+        "base" => "/helpdesk/"
     )
 );
