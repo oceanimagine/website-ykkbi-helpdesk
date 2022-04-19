081aa83a87a93a75a8e3502ae0f29bb37429eba5
Ikhsan Bahar <ikhsanbahar@gmail.com>
2022-04-19 18:20:36 +0700

    Kode Helpdesk

--- config/connect-list.php
+++ config/connect-list.php
@@ -14,13 +14,13 @@ $list_server = array(
         "user" => "root",
         "pass" => "Ykkbi@2021",
         "data" => "project_helpdesk",
-        "base" => "/helpdesk/"
+        "base" => "/ykkbidesk/"
     ),
     "200.60.4.93" => array(
         "host" => "localhost",
         "user" => "root",
         "pass" => "Ykkbi@2021",
         "data" => "project_helpdesk",
-        "base" => "/helpdesk/"
+        "base" => "/ykkbidesk/"
     )
 );
